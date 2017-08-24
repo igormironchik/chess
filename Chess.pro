b32628b7ad08bbae20c02d793e9f801e7ffb9586
Igor Mironchik <igor.mironchik@gmail.com>
2017-08-24 15:13:09 +0300

    Resources.

--- Chess.pro
+++ Chess.pro
@@ -4,6 +4,10 @@ TARGET = chess
 QT += qml
 CONFIG += c++14
 
+win32 {
+    RC_FILE = chess.rc
+}
+
 OTHER_FILES =	Board.qml \
 				Cell.qml \
 				main.qml
--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QIcon>
 
 // Chess include.
 #include "game.hpp"
@@ -34,6 +35,17 @@
 int main( int argc, char ** argv )
 {
 	QGuiApplication app( argc, argv );
+
+	QIcon appIcon( ":/img/icon256x256.png" );
+	appIcon.addFile( ":/img/icon128x128.png" );
+	appIcon.addFile( ":/img/icon64x64.png" );
+	appIcon.addFile( ":/img/icon48x48.png" );
+	appIcon.addFile( ":/img/icon32x32.png" );
+	appIcon.addFile( ":/img/icon22x22.png" );
+	appIcon.addFile( ":/img/icon16x16.png" );
+	appIcon.addFile( ":/img/icon8x8.png" );
+	app.setWindowIcon( appIcon );
+
 
 	QQmlApplicationEngine engine;
 	Chess::Board board;
